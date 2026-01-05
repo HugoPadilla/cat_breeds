c77f68dd45cb5902cace7c7914946a2dc4118293
Hugo Padilla <hugojavier19890@gmail.com>
2026-01-05 18:43:47 -0500

    chore(di): configure dependency injection with injectable
    - Add get_it, injectable, build_runner, and injectable_generator dependencies to pubspec.yaml.
    - Create initial setup files for dependency injection, including app_module and dependency_injector.
    - Configure main.dart to initialize dependencies before the app starts.
    - Generate the initial dependency_injector.config.dart file.

---
+++ lib/di/app_module.dart
@@ -0,0 +1,6 @@
+import 'package:injectable/injectable.dart';
+
+@module
+abstract class AppModule {
+  // TODO: HC 5/01/26 - Here will go the base injections (Dio, etc)
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:cat_breeds/app/app.dart';
+import 'package:cat_breeds/di/dependency_injector.dart';
 import 'package:flutter/material.dart';
 
 void mainApp() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureDependencies();
   runApp(const MyApp());
 }
