b579f2409804b0270b3e279d2023bbfd8190b0f2
Hugo Padilla <hugojavier19890@gmail.com>
2026-01-06 11:19:50 -0500

    refactor(network): abstract http client implementation for datasource
    - Implemented an `ApiClient` abstract class to decouple the datasource from the specific `Dio` HTTP client.
    - Refactored `DioClient` to implement the `ApiClient` interface, ensuring adherence to the new abstraction.
    - Updated the `CatBreedsDatasourceImpl` to depend on the `ApiClient` abstraction instead of the concrete `DioClient`.
    - Adjusted dependency injection bindings to provide the `DioClient` implementation for the `ApiClient` interface.

--- lib/di/dependency_injector.config.dart
+++ lib/di/dependency_injector.config.dart
@@ -12,7 +12,12 @@
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
 
-import '../config/network/dio_client.dart' as _i601;
+import '../core/network/api_client.dart' as _i510;
+import '../core/network/dio_client.dart' as _i393;
+import '../features/cat_breeds/data/datasources/cat_breeds_datasource.dart'
+    as _i471;
+import '../features/cat_breeds/domain/datasources/cat_breeds_datasource.dart'
+    as _i401;
 
 extension GetItInjectableX on _i174.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -21,7 +26,10 @@ extension GetItInjectableX on _i174.GetIt {
     _i526.EnvironmentFilter? environmentFilter,
   }) {
     final gh = _i526.GetItHelper(this, environment, environmentFilter);
-    gh.singleton<_i601.DioClient>(() => _i601.DioClient());
+    gh.lazySingleton<_i510.ApiClient>(() => _i393.DioClient());
+    gh.lazySingleton<_i401.CatBreedsDatasource>(
+      () => _i471.CatBreedsDatasourceImpl(gh<_i510.ApiClient>()),
+    );
     return this;
   }
 }
